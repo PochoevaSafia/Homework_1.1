de6cbbf01a3799670ecc90ff18984b7e6e531220
Safia <918364019s@gmail.com>
2025-08-16 18:19:43 +0300

    Home Work 1

--- ДЗ-2.py
+++ ДЗ-2.py
@@ -1,24 +1,25 @@
 import math
-a = 5
-b = 3
-c = (-26)
-D = b**2 - 4*a*c
-print(D)
-x1 =(-b + math.sqrt(D))/(2*a)
-x2 =(-b - math.sqrt(D))/(2*a)
-print(x1, x2)
+# a = 5
+# b = 3
+# c = (-26)
+# D = b**2 - 4*a*c
+# print(D)
+# x1 =(-b + math.sqrt(D))/(2*a)
+# x2 =(-b - math.sqrt(D))/(2*a)
+# print(x1, x2)
 
-a = 10
-b = 3
-c = 7
+a = int(input())
+b = int(input())
+c = int(input())
 D = b**2 - 4*a*c
 print(D)
 if D > 0:
    x1 =(-b + math.sqrt(D))/(2*a)
    x2 =(-b - math.sqrt(D))/(2*a)
-   print(x1, x2)
+   print("Дискриминант имеет два корня:",x1, x2)
 elif D == 0:
-     x1 =(-b)/(2*a)
+    x1 = (-b + math.sqrt(D)) / (2 * a)
+    print("Один корень:", x1)
 else:
    print ('Дискриминант не имеет корня!')
 
